a8b6a3c245b2e6d4d2ff18bb725aca04a55f0ef5
aaidilz <aidil.233040059@mail.unpas.ac.id>
2025-02-12 11:46:38 +0700

    Enhance linked list example by adding additional nodes and connections

--- P2/nodeMain.java
+++ P2/nodeMain.java
@@ -4,11 +4,28 @@ public class nodeMain {
     public static void main(String[] args) {
 
         // Membuat dua node
+        // Node n1 = new Node(5);
+        // Node n2 = new Node(7);
+        
         Node n1 = new Node(5);
         Node n2 = new Node(7);
+        Node n3 = new Node(9);
+        Node n4 = new Node(8);
 
-        // Menghubungkan dua node
         n1.setNext(n2);
+        n2.setNext(n3);
+        n3.setNext(n4);
+
+        // Node n1 = new Node(2);
+        // Node n2 = new Node(3);
+        // Node n3 = new Node(5);
+        // Node n4 = new Node(7);
+        // Node n5 = new Node(9);
+
+        // n1.setNext(n2);
+        // n2.setNext(n3);
+        // n3.setNext(n4);
+        // n4.setNext(n5);
 
         // Mencetak node
         Node p = n1;
